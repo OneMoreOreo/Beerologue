69d5d3ad03631e2b314c37f2bbd357c93c1c1fff
Markus Juntura <maqayhan@yahoo.com>
2022-08-01 09:42:43 +0200

    -Initial Design
    -Initial Design For First Page

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,81 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <section id="skills" class="py-5 bg-white">
+      <div class="container">
+        <!-- title -->
+        <div class="row">
+          <div class="col text-center">
+            <h1 class="display-4 text-uppercase text-dark mb-0">
+              <strong>Beerolgue Main</strong>
+            </h1>
+            <div class="title-underline bg-danger"></div>
+
+            <p class="mt-2 text-capitalize text-muted">
+              Lorem ipsum recusandae earum est modi ex tempora!
+            </p>
+          </div>
+        </div>
+        <!-- /title -->
+        <div class="row">
+          <!-- single col -->
+          <div class="col-md-6 col-lg-3 text-center my-4">
+            <i class="fab fa-node-js fa-5x text-danger"></i>
+            <h3 class="text-uppercase my-3">Node.Js</h3>
+            <p class="text-muted">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia et
+              eius quibusdam culpa? Adipisci porro odit dolore, a dolorem vel.
+            </p>
+
+            <a href="#" class="btn btn-outline-danger btn-block text-uppercase"
+              >PRESS HERE</a
+            >
+          </div>
+          <!-- /single col -->
+          <!-- single col -->
+          <div class="col-md-6 col-lg-3 text-center my-4">
+            <i class="fab fa-html5 fa-5x text-danger"></i>
+            <h3 class="text-uppercase my-3">HTML5</h3>
+            <p class="text-muted">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia et
+              eius quibusdam culpa? Adipisci porro odit dolore, a dolorem vel.
+            </p>
+
+            <a href="#" class="btn btn-outline-danger btn-block text-uppercase"
+              >PRESS HERE</a
+            >
+          </div>
+          <!-- /single col -->
+          <!-- single col -->
+          <div class="col-md-6 col-lg-3 text-center my-4">
+            <i class="fab fa-js fa-5x text-danger"></i>
+            <h3 class="text-uppercase my-3">Js</h3>
+            <p class="text-muted">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia et
+              eius quibusdam culpa? Adipisci porro odit dolore, a dolorem vel.
+            </p>
+
+            <a href="#" class="btn btn-outline-danger btn-block text-uppercase"
+              >PRESS HERE</a
+            >
+          </div>
+          <!-- /single col -->
+          <!-- single col -->
+          <div class="col-md-6 col-lg-3 text-center my-4">
+            <i class="fab fa-css3 fa-5x text-danger"></i>
+            <h3 class="text-uppercase my-3">CSS</h3>
+            <p class="text-muted">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia et
+              eius quibusdam culpa? Adipisci porro odit dolore, a dolorem vel.
+            </p>
+
+            <a href="#" class="btn btn-outline-danger btn-block text-uppercase"
+              >PRESS HERE</a
+            >
+          </div>
+          <!-- /single col -->
+        </div>
+      </div>
+    </section>
   </div>
 </template>
